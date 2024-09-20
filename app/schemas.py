7d6cfb050d4f93a43adb353e42cedb4a7d9f9a87
Turin0 <163298286+Turin0@users.noreply.github.com>
2024-09-20 13:19:34 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -2,25 +2,23 @@ from pydantic import BaseModel
 
 
 class CreateUser(BaseModel):
-    username = str
-    firstname = str
-    lastname = str
-    age = int
+    username: str
+    firstname: str
+    lastname: str
+    age: int
 
 
 class UpdateUser(BaseModel):
-    firstname = str
-    lastname = str
-    age = int
+    firstname: str
+    lastname: str
+    age: int
 
 
 class CreateTask(BaseModel):
-    title = str
-    content = str
-    priority = int
+    title: str
+    content: str
+    priority: int
 
 
-class UpdateTask(BaseModel):
-    title = str
-    content = str
-    priority = int
+class UpdateTask(CreateTask):
+    pass
